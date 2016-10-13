22d95373f44e52a88628e0188b066c18b284528f
Jackie <quanzhi.huang@scotiabank.com>
2016-10-13 14:46:42 -0400

    Added Upload process

--- app/src/actions/uploads.js
+++ app/src/actions/uploads.js
@@ -11,6 +11,44 @@ export const isFileUploading = (status) => {
     return {type: FILE_UPLOADING, status};
 };
 
+export const runUploadQueue = (files) => {
+
+    if (files.length > 0) {
+        var formData = new FormData();
+
+        // put all files in queue
+        files.map((file, index) => {
+            formData.append('uploads[]', file.data, file.data.name);
+        })
+
+        $.ajax({
+            url: '/',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: (data) => {
+                console.log('upload successful!');
+            },
+            xhr: () => {
+                // create an XMLHttpRequest
+                var xhr = new XMLHttpRequest();
+                // listen to the 'progress' event
+                xhr.upload.addEventListener('progress', function (evt) {
+
+                    if (evt.lengthComputable) {
+                        let percentComplete = evt.loaded / evt.total;
+                        percentComplete = parseInt(percentComplete * 100);
+                        console.log(percentComplete + "%");
+                    }
+                }, false);
+
+                return xhr;
+            }
+        });
+    }
+};
+
 export const setUploadFiles = (files) => {
     // split files and create object
 
